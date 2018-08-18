53c4385aecd0052719e7ec2b188c7c9ff2395b99
Michael Berry <michael.berry@fluxfederation.com>
2018-08-18 15:31:25 +1200

    Help out with task storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen'
-//import { IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Contacts } from '@ionic-native/contacts';
 import { SMS } from '@ionic-native/sms';
@@ -10,15 +10,7 @@ import { CallNumber } from '@ionic-native/call-number';
 import { MyApp } from './app.component';
 
 import { User } from '../providers/user/user';
-// import { User } from '../pages/login/login';
-// import { AffirmationPage } from '../pages/affirmation/affirmation';
-// import { CrisisPage } from '../pages/crisis/crisis';
-// import { HomePage } from '../pages/home/home';
-// import { MenuPage } from '../pages/menu/menu';
-// import { MoodPage } from '../pages/mood/mood';
-// import { SettingsPage } from '../pages/settings/settings';
-// import { TaskPage } from '../pages/task/task';
-// import { VentPage } from '../pages/vent/vent';
+
 
 @NgModule({
   declarations: [
@@ -26,14 +18,15 @@ import { User } from '../providers/user/user';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)//,
-    //IonicStorageModule.forRoot()
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
   ],
   providers: [
+
     Contacts,
     SMS,
     CallNumber,
--- src/pages/task/task.ts
+++ src/pages/task/task.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
 /**
  * Generated class for the TaskPage page.
  *
@@ -15,11 +15,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TaskPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  taskName:string='Eat shit';
+  taskKey:string='';
+  taskDescription:string='to eat some poo';
+  timesCompleted:number=3;
+  taskNameToDisplay:string='';
+
+  constructor(private storage: Storage, public navCtrl: NavController, public navParams: NavParams) {
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TaskPage');
   }
 
+  newTask() {
+    this.taskKey = this.taskName;
+
+    this.storage.set(this.taskKey, {
+      title: this.taskName,
+      description: this.taskDescription,
+      completed: this.timesCompleted
+    });
+    console.log(this.storage.get(this.taskKey));
+  }
+
+  displayTask() {
+    this.storage.get(this.taskKey).then((data)=>{
+      this.var=data;
+      this.taskNameToDisplay = this.var.title;
+    });
+    console.log(this.taskNameToDisplay);
+  }
 }
