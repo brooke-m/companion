c9266b4fa2939cca2c523ff6367796d7fddd942f
MezzoForBezzo <michael.ashdown.berry@gmail.com>
2018-08-22 09:41:24 +1200

    Delete app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig =
-  {
-    apiKey: "AIzaSyDM1Wv_LpL9LhZQi1rG-ucfEANvPQqxTso",
-    authDomain: "comanion-557f7.firebaseapp.com",
-    databaseURL: "https://comanion-557f7.firebaseio.com",
-    projectId: "comanion-557f7",
-    storageBucket: "comanion-557f7.appspot.com",
-    messagingSenderId: "1029762177247"
-  };
